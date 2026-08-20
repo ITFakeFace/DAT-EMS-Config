bc4a65fec0b67c022a450105a8d3396475f3cfbf
ITFakeFace <145489024+ITFakeFace@users.noreply.github.com>
2026-08-20 11:42:12 +0700

    adjust import index, adjust filename, init device screen

--- src/Components/Screens/Dashboard/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = () => {
-  return (
-    <div className="DAT_Dashboard">
-      <h1>Dashboard</h1>
-    </div>
-  );
-};
-
-export default Dashboard;
---
+++ src/Components/Screens/Dashboard/DashboardScreen.jsx
@@ -0,0 +1,9 @@
+const DashboardScreen = () => {
+  return (
+    <div className="DAT_DashboardScreen">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardScreen;
